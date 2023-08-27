fc87d4de2ecb81d9671d46db7528936d04499ca3
Swatantra Yadav <swatantra.s.yadav@slrtce.in>
2023-08-27 19:39:21 +0530

    handling cors

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -25,7 +25,7 @@ const TextEditor = () => {
 
     //creating and disconnecting sockets
     useEffect(() => {
-        const s = io("http://localhost:3001")
+        const s = io("https://google-docs-v2st.onrender.com")
         setSocket(s)
 
         return () => {
